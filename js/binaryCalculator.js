2e3400f4be9aa813c06807b67c02f42199eddfa5
jtertil <jtertil@gmail.com>
2019-03-21 14:15:53 +0100

    calc-logic-implementation

---
+++ js/binaryCalculator.js
@@ -0,0 +1,51 @@
+let btn0 = document.getElementById('btn0');
+let btn1 = document.getElementById('btn1');
+let btnClr = document.getElementById('btnClr');
+let btnEql = document.getElementById('btnEql');
+let btnSum = document.getElementById('btnSum');
+let btnSub = document.getElementById('btnSub');
+let btnMul = document.getElementById('btnMul');
+let btnDiv = document.getElementById('btnDiv');
+let res = document.getElementById('res');
+
+
+btn0.onclick = function () { console.log("0"); res.innerText += "0"; }
+btn1.onclick = function () { console.log("1"); res.innerText += "1"; }
+
+btnSum.onclick = function () { console.log("Sum"); res.innerText += "+"; }
+btnSub.onclick = function () { console.log("Sub"); res.innerText += "-"; }
+btnMul.onclick = function () { console.log("Mul"); res.innerText += "*"; }
+btnDiv.onclick = function () { console.log("Div"); res.innerText += "/"; }
+
+btnClr.onclick = function () { console.log("Clr"); res.innerText = ""; }
+btnEql.onclick = calc;
+
+// function calc() { console.log(res.innerText)}
+function calc() {
+  let input = (res.innerText);
+  // TODO parse input using regex to get array [argument1, operator, argument2]
+
+  let arr = ["1010", "/", "1010"] ;
+  let x = parseInt(arr[0], 2);
+  let y = parseInt(arr[2], 2);
+  let op = arr[1];
+
+  switch (op) {
+    case "+":
+      res.innerText = parseInt(x + y, 10).toString(2);
+      break;
+
+    case "-":
+      res.innerText = parseInt(x - y, 10).toString(2);
+      break;
+
+    case "*":
+      res.innerText = parseInt(x * y, 10).toString(2);
+      break;
+
+    case "/":
+      res.innerText = parseInt(x / y, 10).toString(2);
+      break;
+
+  }
+}
